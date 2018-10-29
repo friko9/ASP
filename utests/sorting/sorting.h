bf1c8c86abb6524c9b302da4582639f2e59b48f0
friko9 <friko9@gmail.com>
2018-10-29 14:15:19 +0100

    test_case::test_sorted add parameter for compare function with strong "less" relation

--- utests/sorting/sorting.h
+++ utests/sorting/sorting.h
@@ -4,6 +4,7 @@
 #include "includes/asp_test/test_set.h"
 
 #include <limits>
+#include <functional>
 #include <vector>
 
 namespace sorting
@@ -16,23 +17,24 @@ namespace sorting
     	using elem_t = int8_t;
 	std::vector<elem_t> data;
     public:
+	static test_result test_sorted(std::vector<elem_t>& v,
+				       std::function<bool(const elem_t,const elem_t)> cmp_in_order = std::less<elem_t>())
+	    {
+		test_result res = v.size()-1;
+		if(v.size() <= 1) return {1,1};
+		for(auto it1=v.begin(),it2=it1+1;it2!=v.end();++it1,++it2)
+		    if( cmp_in_order(*it1,*it2) ) ++res;
+		return res;
+	    }
+    public:
 	void populate_data_allvals_inorder()
 	    {
 		for(elem_t i =std::numeric_limits<elem_t>::min(); i < std::numeric_limits<elem_t>::max(); ++i)
 		    data.push_back(i);
 		data.push_back(std::numeric_limits<elem_t>::max());
 	    }
-    };
 
-    template<typename T>
-    test_result test_sorted(std::vector<T>& v)
-    {
-	test_result res = v.size()-1;
-	if(v.size() <= 1) return {1,1};
-	for(auto it1=v.begin(),it2=it1+1;it2!=v.end();++it1,++it2)
-	    if( *it2 >= *it1 ) ++res;
-	return res;
-    }
+    };
 }
 
 #endif /*SORTING_H*/
