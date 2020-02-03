8cb99852e2183f74544f8b72cef998dc7e80c1e5
friko9 <friko9@gmail.com>
2020-02-03 17:17:15 +0100

    fix(asp_sorting) check if empty container passed

--- includes/asp_sorting/bubble_sort.h
+++ includes/asp_sorting/bubble_sort.h
@@ -1,30 +1,31 @@
 #ifndef BUBBLE_SORT_H
 #define BUBBLE_SORT_H
 
-#include <algorithm>
 #include <vector>
 
 template<typename T>
 void bubble_sort_simple(std::vector<T>& v)
 {
-    const auto its = v.begin();
-    for( auto ite = v.end()-1; ite != its; --ite)
-	for(auto it = its; it!=ite; ++it)
-	    if( *(it+1) < *it )
-		std::iter_swap(it,it+1);
+  if(v.empty()) return;
+  const auto its = v.begin();
+  for( auto ite = v.end()-1; ite != its; --ite)
+    for(auto it = its; it!=ite; ++it)
+      if( *(it+1) < *it )
+	std::iter_swap(it,it+1);
 }
 
 template<typename T>
 void bubble_sort_lastswap(std::vector<T>& v)
 {
-    const auto its = v.begin();
-    for( auto ite = v.end()-1,last_swp=its; ite != its; ite=last_swp,last_swp=its)
-	for(auto it = its; it!=ite; ++it)
-	    if( *(it+1) < *it )
-	    {
-		std::iter_swap(it,it+1);
-		last_swp = it;
-	    }
+  if(v.empty()) return;
+  const auto its = v.begin();
+  for( auto ite = v.end()-1,last_swp=its; ite != its; ite=last_swp,last_swp=its)
+    for(auto it = its; it!=ite; ++it)
+      if( *(it+1) < *it )
+	{
+	  std::iter_swap(it,it+1);
+	  last_swp = it;
+	}
 }
 
 template<typename T>
