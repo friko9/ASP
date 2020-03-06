31a29ad4095ee941a196580ee2e05bc2c7497a1a
friko9 <friko9@gmail.com>
2020-03-06 18:34:54 +0100

    feat(utests) add consolidation header for common definitions

---
+++ utests/includes/utest.h
@@ -0,0 +1,9 @@
+//////////////////////////
+// Unit Test	        //
+// Consolidation header //
+//////////////////////////
+
+#include "./utest_inclusive_range.h"
+#include "./utest_pretty.h"
+#include "./utest_tuple.h"
+#include "./utest_utils.h"
